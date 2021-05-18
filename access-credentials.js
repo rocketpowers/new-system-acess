2a5d0b14bd86dfa234c42f2fa8cf78890b783ca1
rocketpowers <rocketpowerswillian@gmail.com>
2021-05-18 01:00:58 -0300

    pre loader

---
+++ access-credentials.js
@@ -0,0 +1,18 @@
+function acesso(form){
+
+    //filtro letras maisculas convertendo td para minusculas
+    form.nome.value=form.nome.value.toLowerCase()
+    form.senha.value=form.senha.value.toLowerCase()
+    
+    if (form.nome.value=="rambo" && form.senha.value=="123"||
+        form.nome.value=="magaiver" && form.senha.value=="456"||
+        form.nome.value=="rocky" && form.senha.value=="789"){
+    
+        location="pagina2.html"}
+        else{
+            form.nome.value=""
+            form.senha.value=""
+            alert("insert the correct information sr.")
+    
+        }
+    }
